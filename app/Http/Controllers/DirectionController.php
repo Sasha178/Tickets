f0de58120c0e214356d69bc8cc25e0a40767f759
aleksandrWeb <shurik90_08@mail.ru>
2021-02-08 19:49:18 +0300

    Ресурсный контроллер для ввывода информации содержащей в моделях

---
+++ app/Http/Controllers/DirectionController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FromAtoB;
+use App\FromBtoA;
+use Illuminate\Http\Request;
+
+class DirectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+       if ($id == 1) {
+           return json_encode(FromAtoB::all()->pluck('timetable'));
+       }
+        if ($id == 2) {
+            return json_encode(FromBtoA::all()->pluck('timetable'));
+        }
+        if ($id == 3) {
+
+            $ArrayList = array(FromAtoB::all()->pluck('timetable'),
+                FromBtoA::all()->pluck('timetable'));
+            return json_encode($ArrayList);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
