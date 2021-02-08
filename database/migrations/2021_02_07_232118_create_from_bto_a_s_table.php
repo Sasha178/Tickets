fca41c9a9e26c31c8e1fab5271d2ccbac11bbc6b
aleksandrWeb <shurik90_08@mail.ru>
2021-02-08 19:46:42 +0300

    Таблицы напралений

---
+++ database/migrations/2021_02_07_232118_create_from_bto_a_s_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFromBtoASTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('from_bto_a_s', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->string('event_id');
+            $table->time('timetable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('from_bto_a_s');
+    }
+}
