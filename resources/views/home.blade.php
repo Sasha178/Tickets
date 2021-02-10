07a4ceeb5ab043497cbda62135a4767c16fc2fe5
aleksandrWeb <shurik90_08@mail.ru>
2021-02-10 23:40:17 +0300

    Редактирование DOM

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
                                             <div class="d-flex justify-content-between">
                                                 <h5>Выбор направления:</h5>
                                                 <select name="route" id="route" class="custom-select col-md-4">
-                                                    <option value="из A в B">Выбор направления</option>
+                                                    <option value="Направления">Направления</option>
                                                     <option value="из A в B">из A в B</option>
                                                     <option value="из B в A">из B в A</option>
                                                     <option value="из A в B и обратно в А">из A в B и обратно в А
@@ -41,25 +41,22 @@
                                         <div class="col-md-12 m-2">
                                             <div class="d-flex justify-content-between">
                                                 <label for="time"><h5>Купить билет:</h5></label>
-                                                <select name="time" id="time3" class="custom-select col-md-4">
-                                                    <option value="#">Выбрать количество</option>
-                                                    <option value="1">1</option>
-                                                    <option value="2">2</option>
-                                                    <option value="3">3</option>
-                                                    <option value="4">4</option>
-                                                    <option value="3">5</option>
-                                                    <option value="4">6</option>
-                                                </select>
+                                                <h4 class="d-inline-flex justify-content-around">
+                                                    <span class="d-flex align-items-center">-</span>
+                                                    <input type="text" class="form-control col-md-6"
+                                                    placeholder="0">
+                                                    <span class="d-flex align-items-center">+</span>
+                                                </h4>
                                             </div>
                                         </div>
                                         <div class="col-md-12 m-2">
                                             <div class="d-flex justify-content-around">
-                                                <h5>Время в дороге: <span>11:12</span></h5>
-                                                <h5>Стоимость:<span>700 p</span></h5>
+                                                <h5>Время в дороге: <span id="travelTime"> </span></h5>
+                                                <h5>Стоимость:<span id="price"></span></h5>
                                             </div>
                                         </div>
                                         <div class="d-flex justify-content-end">
-                                            <button type="submit" class="btn btn-primary"> Оформить заявку</button>
+                                            <button type="submit" class="btn btn-primary">Посчитать</button>
                                         </div>
                                     </div>
                                 </div>
